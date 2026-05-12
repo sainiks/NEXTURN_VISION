94fb7b15054b46bfc094f9b66e408bc1d28b5847
Kunal Saini <kunalsaini20090360@gmail.com>
2026-05-12 14:59:13 +0530

    remove twitter link from footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,7 +163,6 @@ export default function Home() {
           <div className="flex flex-col gap-4">
             <h4 className="text-xl font-bold uppercase mb-4">Connect</h4>
             <a href="https://www.linkedin.com/company/nexturn-iitm/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className="hover:text-[var(--color-accent)] hover:underline uppercase font-bold tracking-widest">LinkedIn</a>
-            <a href="#" className="hover:text-[var(--color-accent)] hover:underline uppercase font-bold tracking-widest">Twitter</a>
             <a href="https://www.instagram.com/nexturn.iitm/" target="_blank" rel="noopener noreferrer" className="hover:text-[var(--color-accent)] hover:underline uppercase font-bold tracking-widest">Instagram</a>
           </div>
           <div className="text-right">
